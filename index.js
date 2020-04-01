05ddf0981ccd8f0764588edfcecfe85915cd1abb
OlegKaraiman <59221843+OlegKaraiman@users.noreply.github.com>
2020-04-01 10:35:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 function getFibNum(n) {
+    // eslint-disable-next-line no-magic-numbers
+    const arrFibNumb = [0, 1];
     let num1 = 0,
         num2 = 1,
         fib = 1;
-    if (n <= fib) {
-        return fib;
+    if (n < fib) {
+        return arrFibNumb;
     } else {
         for (let i = 1; i <= n; i++) {
             fib = num1 + num2;
+            arrFibNumb.push(fib);
             num1 = num2;
             num2 = fib;
         }
-        return fib;
+        return arrFibNumb;
     }
 }
+
+
+
